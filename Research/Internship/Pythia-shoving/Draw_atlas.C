bee34e9c7b6baf27a137775376309bdc84be3c81
Sanghoon Lim <shlim@pusan.ac.kr>
2020-08-27 09:15:03 +0900

    update template fit figures

--- Research/Internship/Pythia-shoving/Draw_atlas.C
+++ Research/Internship/Pythia-shoving/Draw_atlas.C
@@ -6,12 +6,15 @@ void Draw_atlas(const bool bSAVE=false){
 
 	const int nset = 2;
 	const int nColor[nset] = {2, 4};
+	const float const_pi = TMath::Pi();
 
 	TFile *infile_v22[nset];
 	TFile *infile_v33[nset];
 
 	TFile *infile_v22_pT[nset];
 	TFile *infile_v33_pT[nset];
+
+	TFile *infile_fit[nset];
 
 	infile_v22[0] = new TFile("outfile_v22_mult_pp13TeV_set05set06_grp000_try000.root","read");
 	infile_v22[1] = new TFile("outfile_v22_mult_pp13TeV_set05_grp003_try000.root","read");
@@ -25,11 +28,20 @@ void Draw_atlas(const bool bSAVE=false){
 	infile_v33_pT[0] = new TFile("outfile_v33_pT_pp13TeV_set05set06_grp000_try001.root","read");
 	infile_v33_pT[1] = new TFile("outfile_v33_pT_pp13TeV_set05_grp003_try001.root","read");
 
+	infile_fit[0] = new TFile("outfile_template_fit_pp13TeV_set00_grp000_try010.root","read");
+	infile_fit[1] = new TFile("outfile_template_fit_pp13TeV_set00_grp001_try010.root","read");
+
 	TGraphErrors *gv22[nset];
 	TGraphErrors *gv33[nset];
 
 	TGraphErrors *gv22_pT[nset];
 	TGraphErrors *gv33_pT[nset];
+
+	TH1D *hHM[nset];
+	TH1D *hLM[nset];
+	TF1 *fPED[nset];
+	TF1 *fFLOW[nset];
+	TH1D *hFIT[nset];
 
 	for (int iset=0; iset<nset; iset++){
 		gv22[iset] = (TGraphErrors*)infile_v22[iset]->Get("gv22_sub_nref_3");
@@ -43,6 +55,12 @@ void Draw_atlas(const bool bSAVE=false){
 
 		gv22_pT[iset]->SetLineColor(nColor[iset]);
 		gv33_pT[iset]->SetLineColor(nColor[iset]);
+
+		hHM[iset] = (TH1D*)infile_fit[iset]->Get("hHM_mult8");
+		hLM[iset] = (TH1D*)infile_fit[iset]->Get("hLM_mult8");
+		fPED[iset] = (TF1*)infile_fit[iset]->Get("fPED_mult8");
+		fFLOW[iset] = (TF1*)infile_fit[iset]->Get("fFLOW_mult8");
+		hFIT[iset] = (TH1D*)infile_fit[iset]->Get("hFIT_mult8");
 	}
 
 	ifstream fdata;
@@ -248,6 +266,51 @@ void Draw_atlas(const bool bSAVE=false){
 		leg->Draw();
 	}
 
+	TCanvas *c5[nset];
+
+	for (int iset=0; iset<nset; iset++){
+		c5[iset] = new TCanvas(Form("c5_set%d",iset),Form("c5_set%d",iset),1.1*500,500);
+		SetPadStyle();
+		gPad->SetBottomMargin(0.13);
+		gPad->SetLeftMargin(0.16);
+
+		float max = hHM[iset]->GetMaximum();
+		float min = hHM[iset]->GetMinimum();
+
+		htmp = (TH1D*)gPad->DrawFrame(-const_pi/2, min-0.05*(max-min), 3*const_pi/2, max+0.05*(max-min));
+		SetHistoStyle("#Delta#phi (rad)","Per trigger yield","",24,20);
+		hHM[iset]->Draw("p same");
+		hLM[iset]->Draw("p same");
+		fPED[iset]->Draw("same");
+		fFLOW[iset]->Draw("same");
+		hFIT[iset]->Draw("same");
+
+		{
+			TLegend *leg = new TLegend(0.18,0.4,0.55,0.95);
+			leg->SetFillStyle(0);
+			leg->SetBorderSize(0);
+			leg->SetTextFont(43);
+			leg->SetTextSize(20);
+			le = leg->AddEntry("","pp #sqrt{s}=13 TeV","");
+			if ( iset==0 ){
+				le = leg->AddEntry("","PYTHIA8 default","");
+			}else{
+				le = leg->AddEntry("","PYTHIA8 string shoving","");
+			}
+			le = leg->AddEntry("","Template fit","");
+			le = leg->AddEntry("","0.5<p_{T}^{Assoc}<5.0 GeV/c","");
+			le = leg->AddEntry("","0<|#eta^{Trig}|, |#eta^{Assoc}|<2.5","");
+			le = leg->AddEntry(hHM[iset],"HM 80#leqN_{ch}<90","P");
+			le = leg->AddEntry(hLM[iset],"Scaled LM 0#leqN_{ch}<20","P");
+			le = leg->AddEntry(fPED[iset],"Pedestal","L");
+			le = leg->AddEntry(fFLOW[iset],"Flow","L");
+			le = leg->AddEntry(hFIT[iset],"Total fit","L");
+			leg->Draw();
+		}
+
+	}
+
+
 	if ( bSAVE ){
 		c3->cd();
 		c3->SaveAs("v22-mult.png");
@@ -258,6 +321,7 @@ void Draw_atlas(const bool bSAVE=false){
 		c4_->cd();
 		c4_->SaveAs("v33-pT.png");
 
+		/*
 		TFile *outfile = new TFile("outfile_PYTHIA8_pp13TeV_template_fit.root","recreate");
 		gdata_v22_pT->Write("gdata_v22_pT");
 		gdata_v33_pT->Write("gdata_v33_pT");
@@ -272,6 +336,7 @@ void Draw_atlas(const bool bSAVE=false){
 		gv22[1]->Write("gv22_mult_PYTHIA_shoving");
 		gv33[0]->Write("gv33_mult_PYTHIA");
 		gv33[1]->Write("gv33_mult_PYTHIA_shoving");
+		*/
 	}
 
 }
